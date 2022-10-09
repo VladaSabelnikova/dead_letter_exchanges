45216c66e29920d6e60576e0d6a8491a9906cc3e
Vlada Sabelnikova <vladasabelnikova@gmail.com>
2022-10-09 19:32:13 +0300

    Исправила проблему с импортами
    Теперь корнем является /

--- src/use_case/pub_delayed_message.py
+++ src/use_case/pub_delayed_message.py
@@ -1,7 +1,7 @@
 """Модуль содержит демо функцию, публикующую отложенное сообщение в очередь."""
 import asyncio
 
-from message_brokers.rabbit_message_broker import message_broker_factory
+from src.message_brokers.rabbit_message_broker import message_broker_factory
 
 
 async def publisher() -> None:
--- src/use_case/pub_kill_signal_message.py
+++ src/use_case/pub_kill_signal_message.py
@@ -1,8 +1,8 @@
 """Модуль содержит демо функцию, публикующую kill-signal сообщение в очередь."""
 import asyncio
 
-from config.settings import config
-from message_brokers.rabbit_message_broker import message_broker_factory
+from src.config.settings import config
+from src.message_brokers.rabbit_message_broker import message_broker_factory
 
 
 async def publisher() -> None:
